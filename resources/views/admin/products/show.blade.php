5d51de9440cc7d25c5721b179d1f9cbf0655c72a
samiMeezo <sami.rehman2@meezotech.com>
2025-07-07 12:26:20 +0500

    .

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -26,18 +26,23 @@
         <div class="container mt-3">
           
           <div class="row">
+            <!-- Serial No -->
             <div class="col-md-3 text-center">
               <div class="card">
                 <h5 class="m-0">Serial No</h5>
                 <p>{{$product->serial_no}}</p>
               </div>
             </div>
+
+            <!-- Product Name -->
             <div class="col-md-3 text-center">
               <div class="card">
                 <h5 class="m-0">Product Name</h5>
                 <p>{{$product->product->name}}</p>
               </div>
             </div>
+
+            <!-- Category -->
             <div class="col-md-3 text-center">
               <div class="card">
                 <h5 class="m-0">Category</h5>
@@ -45,13 +50,17 @@
               </div>
             </div>
 
+            <!-- Brands -->
             <div class="col-md-3 text-center">
               <div class="card">
                 <h5 class="m-0">Brands</h5>
                 <p>{{$product->brands}}</p>
               </div>
             </div>
+          </div>
 
+          <div class="row">
+            <!-- Purchase Quantity -->
             <div class="col-md-3 text-center">
               <div class="card">
                 <h5 class="m-0">Purchase Quantity</h5>
@@ -59,6 +68,7 @@
               </div>
             </div>
 
+            <!-- Available Quantity -->
             <div class="col-md-3 text-center">
               <div class="card">
                 <h5 class="m-0">Available Quantity</h5>
@@ -66,6 +76,7 @@
               </div>
             </div>
 
+            <!-- Price -->
             <div class="col-md-3 text-center">
               <div class="card">
                 <h5 class="m-0">Price</h5>
@@ -73,6 +84,7 @@
               </div>
             </div>
 
+            <!-- Alarm Quantity -->
             <div class="col-md-3 text-center">
               <div class="card">
                 <h5 class="m-0">Alarm Quantity</h5>
@@ -81,16 +93,60 @@
             </div>
           </div>
 
+          <div class="row">
+            <!-- Supplier -->
+            <div class="col-md-3 text-center">
+              <div class="card">
+                <h5 class="m-0">Supplier</h5>
+                <p>{{$product->supplier->name}}</p>
+              </div>
+            </div>
+
+            <!-- Warehouse -->
+            <div class="col-md-3 text-center">
+              <div class="card">
+                <h5 class="m-0">Warehouse</h5>
+                <p>{{$product->warehouse->name}}</p>
+              </div>
+            </div>
+
+            <!-- Procured Date -->
+            <div class="col-md-3 text-center">
+              <div class="card">
+                <h5 class="m-0">Procured Date</h5>
+                <p>{{\Carbon\Carbon::parse($product->procured_date)->format('d-M-Y')}}</p>
+              </div>
+            </div>
+
+            <!-- Expiry Date -->
+            <div class="col-md-3 text-center">
+              <div class="card">
+                <h5 class="m-0">Expiry Date</h5>
+                <p>{{\Carbon\Carbon::parse($product->expiry_date)->format('d-M-Y')}}</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <!-- Description -->
+            <div class="col-md-6 text-center">
+              <div class="card">
+                <h5 class="m-0">Description</h5>
+                <p>{{$product->description}}</p>
+              </div>
+            </div>
+          </div>
+
           <div class="col-md-12">
             <label for=""></label>
             <div class="text-right">
               <a href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-warning">Edit</a>
-                <a href="{{ route('admin.products.index') }}" class="btn btn-secondary">Back</a>
-                <form action="{{ route('admin.products.destroy', $product->id) }}" method="POST" style="display:inline-block;" onsubmit="return confirm('Are you sure you want to delete this Product?');">
-                  @csrf
-                  @method('DELETE')
-                  <button class="btn btn-danger">Delete</button>
-                </form>
+              <a href="{{ route('admin.products.index') }}" class="btn btn-secondary">Back</a>
+              <form action="{{ route('admin.products.destroy', $product->id) }}" method="POST" style="display:inline-block;" onsubmit="return confirm('Are you sure you want to delete this Product?');">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger">Delete</button>
+              </form>
             </div>
             <br>
           </div>
