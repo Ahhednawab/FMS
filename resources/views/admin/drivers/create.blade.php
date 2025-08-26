b3282ca8dbc9489de3f2038621c81bde1d5adf74
samiMeezo <sami.rehman2@meezotech.com>
2025-08-26 11:39:43 +0500

    .

--- resources/views/admin/drivers/create.blade.php
+++ resources/views/admin/drivers/create.blade.php
@@ -3,6 +3,14 @@
 @section('title', 'Add Driver')
 
 @section('content')
+
+  <style>
+   .select2-search--dropdown::after {
+      content: '' !important;
+      display: none !important;
+      background: none !important;
+    }
+  </style>
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@1.5.2/dist/select2-bootstrap4.min.css" rel="stylesheet" />
 
